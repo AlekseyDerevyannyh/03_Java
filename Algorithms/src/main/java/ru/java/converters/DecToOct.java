8bfe778ae6d75dc5926cbf80ddada10d633b8594
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-12-05 15:41:15 +0300

    add Algorithm DecToOct method decToOct

--- Algorithms/src/main/java/ru/java/converters/DecToOct.java
+++ Algorithms/src/main/java/ru/java/converters/DecToOct.java
@@ -3,6 +3,7 @@ package ru.java.converters;
 public class DecToOct {
     public static void main(String[] args) {
         System.out.println(decToOct(753));
+        System.out.println(octToDec(10));
     }
 
     public static int decToOct(int number) {
@@ -14,6 +15,18 @@ public class DecToOct {
             i++;
         }
         result += (number % 8) * (int) Math.pow(10, i);
+        return result;
+    }
+
+    public static int octToDec(int number) {
+        int result = 0;
+        int i = 0;
+        while (number / 10 > 0) {
+            result += (number % 10) * (int) Math.pow(8, i);
+            number /= 10;
+            i++;
+        }
+        result += (number % 10) * (int) Math.pow(8, i);
         return result;
     }
 }
