511eb7693dd9582b1c3a3be970532431d49244b7
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-12-06 22:06:33 +0300

    modify Algorithms DecToOct & DecToBin

--- Algorithms/src/main/java/ru/java/converters/DecToBin.java
+++ Algorithms/src/main/java/ru/java/converters/DecToBin.java
@@ -3,7 +3,9 @@ package ru.java.converters;
 public class DecToBin {
     public static void main(String[] args) {
         System.out.println(decToBin(23));
+        System.out.println(decToBin1(23));
         System.out.println(binToDec(1011001));
+        System.out.println(binToDec("1011001"));
     }
 
     public static int decToBin(int number) {
@@ -12,12 +14,23 @@ public class DecToBin {
         }
         int result = 0;
         int i = 0;
-        while (number / 2 > 0) {
+        while (number > 0) {
             result += (number % 2) * (int) Math.pow(10, i);
             number /= 2;
             i++;
         }
-        result += (number % 2) * (int) Math.pow(10, i);
+        return result;
+    }
+
+    public static String decToBin1(int number) {
+        if (number <= 0) {
+            return "";
+        }
+        String result = "";
+        while (number > 0) {
+            result = number % 2 + result;
+            number /= 2;
+        }
         return result;
     }
 
@@ -27,12 +40,22 @@ public class DecToBin {
         }
         int result = 0;
         int i = 0;
-        while (number / 10 > 0) {
+        while (number > 0) {
             result += (number % 10) * (int) Math.pow(2, i);
             number /= 10;
             i++;
         }
-        result += (number % 10) * (int) Math.pow(2, i);
+        return result;
+    }
+
+    public static int binToDec(String number) {
+        if (number == null || number.isEmpty()) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < number.length(); i++) {
+            result += Character.getNumericValue(number.charAt(number.length() -1 - i)) * (int) Math.pow(2, i);
+        }
         return result;
     }
 }
--- Algorithms/src/main/java/ru/java/converters/DecToOct.java
+++ Algorithms/src/main/java/ru/java/converters/DecToOct.java
@@ -12,27 +12,25 @@ public class DecToOct {
         }
         int result = 0;
         int i = 0;
-        while (number / 8 > 0) {
+        while (number > 0) {
             result += (number % 8) * (int) Math.pow(10, i);
             number /= 8;
             i++;
         }
-        result += (number % 8) * (int) Math.pow(10, i);
         return result;
     }
 
     public static int octToDec(int number) {
-        if (number <=0) {
+        if (number <= 0) {
             return 0;
         }
         int result = 0;
         int i = 0;
-        while (number / 10 > 0) {
+        while (number > 0) {
             result += (number % 10) * (int) Math.pow(8, i);
             number /= 10;
             i++;
         }
-        result += (number % 10) * (int) Math.pow(8, i);
         return result;
     }
 }
