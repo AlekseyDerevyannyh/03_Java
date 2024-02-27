c0296cd8cb9c8b67e3e1c0a6ff1c88e66347f50c
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-02-27 22:14:49 +0300

    modify HexToBin

--- Algorithms/src/main/java/ru/java/converters/HexToBin.java
+++ Algorithms/src/main/java/ru/java/converters/HexToBin.java
@@ -1,6 +1,7 @@
 package ru.java.converters;
 
 public class HexToBin {
+    
     public static final String HEX = "0123456789abcdef";
     public static final String[] BIN = {"0000", "0001", "0010", "0011", "0100", "0101",
     "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
