188fcb944e9e04fb157af6738e1a9fd66b8c53a3
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-08-28 13:22:41 +0300

    update DecToBin.java

--- Algorithms/src/main/java/ru/java/converters/DecToBin.java
+++ Algorithms/src/main/java/ru/java/converters/DecToBin.java
@@ -1,7 +1,6 @@
 package ru.java.converters;
 
 public class DecToBin {
-
     public static void main(String[] args) {
         System.out.println(decToBin(23));
         System.out.println(decToBin1(23));
